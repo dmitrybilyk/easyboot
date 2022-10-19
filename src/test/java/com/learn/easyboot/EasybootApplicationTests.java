33f0f2eb527da347704050cc515dd84f59dacc5c
dmytro <dmitry.b@scopicsoftware.com>
2022-10-19 21:23:22 +0300

    initial commit

---
+++ src/test/java/com/learn/easyboot/EasybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.easyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

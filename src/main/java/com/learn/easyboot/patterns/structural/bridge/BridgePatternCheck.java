9370fc072055b51f95962832cd3dc8c3cc6e1922
dmytro <dmitry.b@scopicsoftware.com>
2022-12-15 20:27:17 +0200

    bridge check

---
+++ src/main/java/com/learn/easyboot/patterns/structural/bridge/BridgePatternCheck.java
@@ -0,0 +1,52 @@
+
+public class BridgePatternCheck {
+	public static void main(String[] args) {
+		Driver driver = new ExperiencedDriver();
+		Car car = new LuxuryCar(driver);
+		car.drive();
+	}
+}
+
+abstract class Car {
+	abstract void drive();
+}
+
+interface Driver {
+	void driveCar();
+}
+
+class CheapCar extends Car {
+	private Driver driver;
+	public CheapCar(Driver driver) {
+		this.driver = driver;
+	}
+	public void drive() {
+		System.out.println("Cheap car id driving");
+		driver.driveCar();
+	}
+}
+
+
+class LuxuryCar extends Car {
+	private Driver driver;
+	public LuxuryCar(Driver driver) {
+		this.driver = driver;
+	}
+	public void drive() {
+		System.out.println("Luxury car id driving");
+		driver.driveCar();
+	}
+}
+
+class NewbeDriver implements Driver {
+	public void driveCar() {
+		System.out.println("Newbe driver is riding");
+	}
+}
+
+
+class ExperiencedDriver implements Driver {
+	public void driveCar() {
+		System.out.println("Experienced driver is riding");
+	}
+}

a15f3da9b54f58b33bfcab117258cfd983ba43eb
dmytro <dmitry.b@scopicsoftware.com>
2022-12-21 22:38:29 +0200

    facade and flyweight

---
+++ src/main/java/com/learn/easyboot/patterns/structural/decorator/DecoratorPattern.java
@@ -0,0 +1,51 @@
+
+public class DecoratorPattern {
+	public static void main(String[] args) {
+		TShirtProducer tShirtProducer = new WhaterProofTshirtDecorator(
+			new ColorfulTshirtDecorator(
+				new SimpleTShirtProducer()));
+		tShirtProducer.produceTShirt();
+	}
+}
+
+interface TShirtProducer {
+	void produceTShirt();
+}
+
+class SimpleTShirtProducer implements TShirtProducer {
+	public void produceTShirt() {
+		System.out.println("Simple tshirt is produced");
+	}
+}
+
+class BaseTShirtProducerDecorator implements TShirtProducer {
+	protected TShirtProducer tShirtProducer;
+	public BaseTShirtProducerDecorator(TShirtProducer tShirtProducer) {
+		this.tShirtProducer = tShirtProducer;
+	}
+
+	public void produceTShirt() {
+		tShirtProducer.produceTShirt();
+	}
+}
+
+class ColorfulTshirtDecorator extends BaseTShirtProducerDecorator {
+	public ColorfulTshirtDecorator(TShirtProducer tShirtProducer) {
+		super(tShirtProducer);
+	}
+	public void produceTShirt() {
+		super.produceTShirt();
+		System.out.println("Added color");
+	}
+}
+
+
+class WhaterProofTshirtDecorator extends BaseTShirtProducerDecorator {
+	public WhaterProofTshirtDecorator(TShirtProducer tShirtProducer) {
+		super(tShirtProducer);
+	}
+	public void produceTShirt() {
+		super.produceTShirt();
+		System.out.println("Added whater proof feature");
+	}
+}

527202cd6232e91664a3b532b88df1db63788468
dmytro <dmitry.b@scopicsoftware.com>
2023-08-09 12:10:17 +0000

    added docker file

--- src/main/java/com/learn/easyboot/models/Human.java
+++ src/main/java/com/learn/easyboot/models/Human.java
@@ -1,5 +1,6 @@
 package com.learn.easyboot.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.io.Serializable;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Human implements Serializable {
     private String name;
 }

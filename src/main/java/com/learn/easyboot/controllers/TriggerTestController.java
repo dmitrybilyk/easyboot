7a3eb0180f04bbdb208e744579714a1a051a440c
dmytro <dmitry.b@scopicsoftware.com>
2023-08-09 11:38:55 +0000

    added docker file

---
+++ src/main/java/com/learn/easyboot/controllers/TriggerTestController.java
@@ -0,0 +1,21 @@
+package com.learn.easyboot.controllers;
+
+import com.learn.easyboot.models.Human;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TriggerTestController {
+
+    @GetMapping("/trigger")
+    public Human getHuman() {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "http://localhost:8707/easy/get";
+        ResponseEntity<Human> response
+                = restTemplate.getForEntity(fooResourceUrl, Human.class);
+        return response.getBody();
+    }
+}

b9be63deabd96915e216e1b2abb29b8eb00ebb4f
dmytro <dmitry.b@scopicsoftware.com>
2023-09-13 13:54:00 +0000

    added config map etc.

--- src/main/java/com/learn/easyboot/controllers/TestController.java
+++ src/main/java/com/learn/easyboot/controllers/TestController.java
@@ -4,6 +4,7 @@ import com.learn.easyboot.dao.UserDAO;
 import com.learn.easyboot.models.Human;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,12 @@ public class TestController {
     @Autowired(required = false)
     private UserDAO userDAO;
 
+    @Value("${params.parameter2}")
+    private String name;
+
     @GetMapping("/get")
     public Human getHuman() {
 //        userDAO.getAllUserNames().forEach(System.out::println);
-        return Human.builder().name("some name").build();
+        return Human.builder().name("some name2 - " + name).build();
     }
 }

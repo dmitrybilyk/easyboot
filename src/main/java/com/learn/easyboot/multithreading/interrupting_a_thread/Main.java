e451a1ef0f66affcfc0b4a07b72d0d109cdad31d
dmytro <dmitry.b@scopicsoftware.com>
2024-01-11 10:52:21 +0200

    interrupted

--- src/main/java/com/learn/easyboot/multithreading/interrupting_a_thread/Main.java
+++ src/main/java/com/learn/easyboot/multithreading/interrupting_a_thread/Main.java
@@ -6,23 +6,29 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread workerThread = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    Thread.sleep(11000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+//                try {
+                    IntStream.range(0, 10000000).forEach(System.out::println);
+//                    Thread.sleep(3000);
+//                } catch (InterruptedException e) {
+////                    System.out.println("interrupt exception");
+//                    throw new RuntimeException(e);
+//                }
                 // Long running task
-                IntStream.range(0, 100).forEach(System.out::println);
+//                IntStream.range(0, 100).forEach(System.out::println);
             }
             System.out.println("Thread is interrupted and exiting.");
         });
 
         workerThread.start();
 
-        // we let it work for 10 seconds
-        Thread.sleep(10000);
+        // we let it work for 1 second
+        Thread.sleep(1000);
 
-// Later in the code, we can stop the thread by interrupting it
-        workerThread.interrupt();
+        // Later in the code, we can stop the thread by interrupting it
+//        try {
+            workerThread.interrupt();
+//        } catch (RuntimeException e) {
+//            System.out.println("Runtime exception");
+//        }
     }
 }

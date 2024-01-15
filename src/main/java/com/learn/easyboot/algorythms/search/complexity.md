5c39c243ed7c5d3625034a1e493c2df1653f2490
dmytro <dmitry.b@scopicsoftware.com>
2024-01-15 20:10:43 +0200

    search

---
+++ src/main/java/com/learn/easyboot/algorythms/search/complexity.md
@@ -0,0 +1,5 @@
+There 2 two types of complexity measures:
+- time complexity - how much time it will take
+- space complaxity - how much memory it could consume
+
+Measured as: O(1), O(n), O(n2) etc.
--- src/main/java/com/learn/easyboot/algorythms/search/linear_search/LinearSearchExample.java
+++ src/main/java/com/learn/easyboot/algorythms/search/linear_search/LinearSearchExample.java
@@ -1,4 +1,6 @@
-public class LinearSearchExample{    
+package com.learn.easyboot.algorythms.search.linear_search;
+
+public class LinearSearchExample{
 public static int linearSearch(int[] arr, int key){    
         for(int i=0;i<arr.length;i++){    
             if(arr[i] == key){    

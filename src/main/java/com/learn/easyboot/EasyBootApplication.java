6cb1701e46ed2d80f03df6cd4812829a54a7df50
dmytro <dmitry.b@scopicsoftware.com>
2024-02-01 18:07:19 +0200

    spring boot

--- src/main/java/com/learn/easyboot/EasyBootApplication.java
+++ src/main/java/com/learn/easyboot/EasyBootApplication.java
@@ -2,9 +2,13 @@ package com.learn.easyboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.learn.easyboot.dao.repositories")
+@EntityScan("com.learn.easyboot.models.entities")
 //@ComponentScan
 //@ComponentScan("com.learn.easyboot.controllers")
 // @Import(AppConfig.class)

3a8876120edae7b2cc8342776e47756e819af984
dmytro <dmitry.b@scopicsoftware.com>
2024-02-16 11:12:31 +0200

    added keycloak

--- src/main/java/com/learn/easyboot/configuration/security/SecurityConfig.java
+++ src/main/java/com/learn/easyboot/configuration/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.learn.easyboot.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+//@Profile("security")
 //@EnableWebSecurity
 public class SecurityConfig {
 

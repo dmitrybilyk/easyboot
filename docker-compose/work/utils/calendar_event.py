3ad291d856568554355a07cc02664b53516f93fc
dmytro <dmitry.b@scopicsoftware.com>
2024-06-05 12:57:22 +0300

    google calendar event

--- docker-compose/work/utils/calendar_event.py
+++ docker-compose/work/utils/calendar_event.py
@@ -9,35 +9,32 @@ from googleapiclient.discovery import build
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
-def main(event_summary):
-    """Shows basic usage of the Google Calendar API.
-    Creates a Google Calendar API service object and adds an event.
-    """
+def get_credentials():
+    """Get credentials, either by loading from token.json or performing OAuth2 flow."""
     creds = None
 
-    # Check if the credentials are available in environment variables
-    client_id = '32660785819-rujpth3h7o7ihj7hi5fsj2p68uj0pq4o.apps.googleusercontent.com'
-    client_secret = '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'
-    refresh_token = os.getenv('GOOGLE_REFRESH_TOKEN')
+    # Check if token.json exists and load credentials from it
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-    if client_id and client_secret and refresh_token:
-        creds = Credentials(
-            token=None,
-            refresh_token=refresh_token,
-            token_uri='https://oauth2.googleapis.com/token',
-            client_id=client_id,
-            client_secret=client_secret,
-            scopes=SCOPES
-        )
-    else:
-        # If no valid credentials are available, let the user log in.
-        flow = InstalledAppFlow.from_client_secrets_file(
-            '/home/dmytro/Dropbox/pers/google_api/calendar/client_secret_32660785819-rujpth3h7o7ihj7hi5fsj2p68uj0pq4o.apps.googleusercontent.com(1).json', SCOPES)
-        creds = flow.run_local_server(port=0)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/home/dmytro/Dropbox/pers/google_api/calendar/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    return creds
+
+def main(event_summary):
+    """Creates a Google Calendar API service object and adds an event."""
+    creds = get_credentials()
     service = build('calendar', 'v3', credentials=creds)
 
     # Calculate start and end times
